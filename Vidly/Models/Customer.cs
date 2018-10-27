2a510105bf88b00bc2b37f435e286d8153d0df97
Jean Santos <jeansantosc@gmail.com>
2018-10-26 21:21:41 -0300

    custom validation messages

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,9 +9,11 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
         public DateTime? Birthdate { get; set; }
         public MembershipType MembershipType { get; set; }
