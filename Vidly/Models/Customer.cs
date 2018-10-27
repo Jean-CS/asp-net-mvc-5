508477793fbfc659ceefb5e527ad363b30dca0ea
Jean Santos <jeansantosc@gmail.com>
2018-10-26 21:27:32 -0300

    add custom data annotations validation for birthdate

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -15,6 +15,8 @@ namespace Vidly.Models
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
+
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
